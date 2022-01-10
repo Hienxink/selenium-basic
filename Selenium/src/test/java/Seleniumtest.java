97e1a1aa47afa87e0cf7cf7eabb0cd41de9254b5
Hien <thuhien.iidcc@gmail.com>
2022-01-10 14:16:39 +0700

    init

--- Selenium/src/test/java/Seleniumtest.java
+++ Selenium/src/test/java/Seleniumtest.java
@@ -1,49 +1,54 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Seleniumtest {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","/Users/hienxink/Desktop/Drivers/chromedriver");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://tiki.vn/");
+    public static void main(String[] args) throws InterruptedException {
+       ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.get("https://www.browserstack.com/");
+//        driver.get("https://www.tiki.vn");
         String title = driver.getTitle();
         System.out.println(title);
 
-        WebElement sachtuduy_kynangsong = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/header/div[1]/div/"
-                +"div[1]/div[2]/div/button"));
-        sachtuduy_kynangsong.click();
+//        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,1000)");
+//        Thread.sleep(3000);
+//        WebElement Sach = driver.findElement(By.xpath("//div[text()='Sách tư duy - Kỹ năng sống']"));
+//        Thread.sleep(3000);
+//        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", Sach);
+//        Sach.click();
+//        Thread.sleep(3000);
+//        WebElement Developers = driver.findElement(By.xpath("//span[text()='Developers']"));
+//        Developers.click();
+//        Thread.sleep(3000);
+//
+//        WebElement link_Docs = driver.findElement(By.linkText("Documentation"));
+//        link_Docs.click();
+//        Thread.sleep(3000);
 
-        WebElement imgBook = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/main/div/div[3]/div/div[1]/div[2]"
-                +"/div/a/div[1]/img"));
-        imgBook.click();
+          WebElement developers = driver.findElement(By.id("developers-menu-toggle"));
+          developers.click();
+          Thread.sleep(500);
 
+          WebElement docs = driver.findElement(By.xpath("//*[@id=\"developers-menu-dropdown\"]/li[1]/a"));
+          docs.click();
+          Thread.sleep(500);
 
-        WebElement soluong = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/main/div[3]/div[1]/div[3]/div[2]"
-                +"/div[1]/div[4]/div[1]/div/div/button[2]/img"));
-        soluong.click();
-        soluong.click();
+          WebElement abc = driver.findElement(By.id("developers-menu-toggle"));
+          abc.click();
+          Thread.sleep(500);
 
-        WebElement xemShop = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/main/div[3]/div[1]/div[3]/div[2]"
-                +"/div[2]/div[1]/div[1]/div[3]/a/span"));
-        xemShop.click();
+          WebElement products = driver.findElement(By.id("product-menu-toggle"));
+          products.click();
+          Thread.sleep(500);
 
-        WebElement theoDoi = driver.findElement(By.xpath("//*[@id=\"seller-info-wrapper\"]/div[3]/button"));
-        theoDoi.click();
-
-        WebElement xinChao = driver.findElement(By.xpath("//*[@name=\"tel\"]"));
-        xinChao.sendKeys("0987114577");
-
-        WebElement dangNhap1 = driver.findElement(By.xpath("//*[@type=\"password\"]"));
-        dangNhap1.sendKeys("123456");
-
-        WebElement hien = driver.findElement(By.xpath("//*[@ class=\"show-pass\"]"));
-        hien.click();
-
-        WebElement dangNhap2 = driver.findElement(By.xpath("//button[contains(text(),'Đăng Nhập')]"));
-        dangNhap2.click();
-
-        WebElement quenMatkhau = driver.findElement(By.xpath("//p[contains(text(),'Quên mật khẩu?')]"));
-        quenMatkhau.click();
+          WebElement live = driver.findElement(By.xpath("//div[@class=\"dropdown-link-heading\"]"));
+          live.click();
+          Thread.sleep(500);
 
         driver.quit();
     }
